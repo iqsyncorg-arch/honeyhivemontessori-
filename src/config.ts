97e86707db88a5adbbafff8931a9b41f51c5abd7
SANJAY NAVEEN <sanjaynaveen@SANJAYs-MacBook-Air.local>
2026-03-30 00:55:57 +0530

    new commits

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 //export const API_URL = "https://honeyhivemontessori-backend.onrender.com/api";
-export const API_URL = "http://localhost:3001/api";
+export const API_URL = "http://api.honeyhivemontessorihouse.com/api";
